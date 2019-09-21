ab913244da81adf6b0f80d4d598888ef5c45d70f
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2019-09-21 22:49:32 +0200

    ad

--- FacebookAddFriendAuto/inject.js
+++ FacebookAddFriendAuto/inject.js
@@ -12,7 +12,7 @@
 		lsClass = document.getElementsByClassName("FriendRequestAdd");
 		// lsClass = document.getElementsByClassName("friendBrowserNameTitle");
 
-		if (index >= maxRequest) {
+		if (index >= lsClass.length) {
 			console.log("Thats enough");
 			return;
 		}
@@ -52,6 +52,56 @@
 		},1000);
 	}
 
+	function runAcceptFriend(index) {
+		lsClass = document.getElementsByClassName("ruResponseButtons");
+		// lsClass = document.getElementsByClassName("friendBrowserNameTitle");
+
+		if (index >= 2) {
+			console.log("Thats enough");
+			return;
+		}
+
+		// if ($("button").hasClass("layerConfirm")) {
+		// 	console.log("Stoppppppp");
+		// 	$("layerConfirm").click();
+		// 	return;
+		// }
+
+		var layerConfirm = document.getElementsByClassName("layerConfirm");
+
+		//If it isn't "undefined" and it isn't "null", then it exists.
+		if(layerConfirm.length > 0){
+			console.log("Stoppppppp");
+			console.log(layerConfirm);
+		    return;
+		}
+
+		setTimeout(function(){
+			console.log("click:"+index);
+			var objChosen = lsClass[index].childNodes[0].childNodes[0];
+			objChosen.focus();
+			console.log(objChosen);
+			var cName = objChosen.className;
+			console.log(cName + " "+ cName.includes("hidden"))
+			if (!cName.includes("hidden")) {
+				// console.log(objChosen);
+				// var name = objChosen.parentElement.parentElement.childNodes[0].textContent;
+				var name = objChosen.parentElement.parentElement.parentElement.parentElement.childNodes[0].childNodes[0].childNodes[0].childNodes[0].textContent;
+				console.log(name);
+				var test = name.toLowerCase().replace(/[^0-9a-zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ\s]/gi, '');
+				if (name.toLowerCase() == test) {
+					console.log("test done: "+test);
+					objChosen.click();
+					// lsClass[index].hover();
+				}
+			}
+			runAcceptFriend(index+1);
+		},1000);
+	}
+
+	// runAddFriend(0);
+	runAcceptFriend(0);
+
 	// var numOfScroll = 0;
 
 	// function scrollToButtom(index){
@@ -68,6 +118,6 @@
 
 	// }
 	// scrollToButtom(0);
-	runAddFriend(0);
+	// runAddFriend(0);
 	// console.log(lsClass)
 })();
